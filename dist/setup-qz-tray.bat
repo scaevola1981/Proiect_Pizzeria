65b60cd8d4055b71ba2c3d7db1e803f02b0cb9ec
Florin Dorobantu <dorobantuflorin81@gmail.com>
2026-08-17 11:35:59 +0300

    Fix setup-qz-tray.bat: create folders automatically, use correct certificate, check multiple install paths

--- dist/setup-qz-tray.bat
+++ dist/setup-qz-tray.bat
@@ -7,75 +7,64 @@ echo   BELLA ROMA - Configurare Imprimanta QZ Tray
 echo ============================================
 echo.
 
-:: Calea QZ Tray pe Windows
+:: Calea QZ Tray pe Windows - creeaza daca nu exista
 set "QZ_PATH=%APPDATA%\QZ Tray"
 set "SSL_PATH=%QZ_PATH%\sslcert"
 
-echo [1/4] Se verifica QZ Tray...
-if not exist "%QZ_PATH%" (
-    echo.
-    echo EROARE: QZ Tray nu este instalat!
-    echo Descarca QZ Tray de pe: https://qz.io/download/
-    echo Instaleaza-l si ruleaza din nou acest script.
-    echo.
-    pause
-    exit /b 1
-)
-echo       OK - QZ Tray gasit la: %QZ_PATH%
-
-echo [2/4] Se creeaza folderul certificat...
+echo [1/3] Se creeaza folderele necesare...
+if not exist "%QZ_PATH%" mkdir "%QZ_PATH%"
 if not exist "%SSL_PATH%" mkdir "%SSL_PATH%"
-echo       OK - Folder creat: %SSL_PATH%
+echo       OK - Foldere create: %SSL_PATH%
 
-echo [3/4] Se instaleaza certificatul digital...
+echo [2/3] Se instaleaza certificatul digital...
 (
 echo -----BEGIN CERTIFICATE-----
-echo MIIEpAIBAAKCAQEAyIlOlsiH0rt8+x5TodLsyrlhKhDKVKqtRIbxKgr+U2AdNaT7
-echo fRAzDCaYvLYNgNxyjoBMBRt97UTIIUY35oHBVMq4AwVwJQEe81MxoLJpV8PerYsS
-echo UuoaV0hfnGQjTjHgSfLpZvABZ6XFD6VwCvg2u9JRsBDptdNaR1hIBmIwM7qNRill
-echo 2LnAbqdVqOFIzd/sky1qywD+2/bi54rBf0ml8SNgw/9V0hvSXeGGYgV/u2KN+HAW
-echo Nt2/sKdwdurdchak7O5YqgNvqPx01JvkO/NhIGLd11LrbPbineNiXQuHwKQ1QVWQ
-echo NcNHgIQ5JIFQuXNUMH8LZFs4KGZLnRL/7FpLOwIDAQABAoIBAQCkxOzlx26SH6rZ
-echo Slm9JOmayCalwZX9ax9ipt2Qhefh6Z8WbLCWWbEX0r68j3kY4AjgPVo4+BXH1jP5
-echo 4xAbPZH0cXwwP0+dmAYuN7UXLICRtEZKoXI03lU2Uij8/upjXWfEWuqbwafl2bbI
-echo 3E3rNXcDbBPiboMY+se6xzamyBaC6NdQRUB3/w6Pd90VCIQOVhfIfLXxArPhDYGF
-echo d/pTRk/B4v/VlbIaZSXE0cqtPydUXSMMZnV8xsa9kHkBJ91vvErcVJpGlsDVbVnJ
-echo u+5EhicPh0pNthq0hgdvtE83PcOP70LUYQNxjurq2AOplnJ5btouUW/cqotE6QPS
-echo P1UWk6LBAoGBAPeFQA3ywSWl9IEUnVN2krtEL+fJaLHO4fM4aNdIfxBentI7zkui
-echo 7fhMM7gt+xJIYOcWamKS4zfwgCCWq0xX+M9ZplrG/aof99M9r0H4lgH2q7YLdr6b
-echo 88Yf/RNWr9I8+LaHM/zivdSxa3lxFF3+nyoRhJjRIv3XBF+kM54QcQ6HAoGBAM9o
-echo AbgPDIpv0q4uPAEZQnAvDyqg373gieAdTpEMnBfyzeBM1XU1a4phb6l3zoq3yXEi
-echo jpyKBRYzlmAIQT84Ck+exQ0CPiOloKVvaz7hfB+VNFnwJD7en91KsH9FlVqM5gO/
-echo x6DaEK9ZXuOFNJodD8h3PFRPUb+MzsTIXZQvxTatAoGBALVDlgcg8aWahRZKfHR0
-echo 7zvI0bRS4SLluL6fXtfZtYPNZ03aklb9uHwPggitU6Kt8pkI51vM6i07KPm0nTnJ
-echo auKeap8r/vQpeRnvoHsVivVhKZqlho5MMxeysWkKILQ8Bn/VP5NAkXhDfctvrlSv
-echo dOwf7BTlg1SVtBQ+cbadn83dAoGADmvv+qlpONMPtjbWy0jDWuOazV8ET8KmM6Q2
-echo C1XyIKQsdpVBHnZJdQTTa9g9z116L1i0y+O4+NM7eI/6YXf3F5Q1pXLreTUSF47I
-echo yUWKiPOqATr2ejympw+DeEYRXYuAjvAt5FxlXpv7QhzIDJNKvqiz1DTzvTsAQaSh
-echo BSXRM70CgYBWWaBpuCCxsuPZVrcV6BT5HjRuXPQ/VUPa7m7MJfljpmiZKn85wAR2
-echo e102YBQ+sbWSIXaRcbVgQlUTarpeBepCZ8CemuwU6v52qFR1vFqzrT8Us9j5il9u
-echo cmislyBkYbexYBYwSSgtxKvsqYU0WbuHsvfrcCDvi/itEXvytisfWA==
+echo MIIC/DCCAeQCCQCa8tjrVFTdTzANBgkqhkiG9w0BAQsFADBAMRwwGgYDVQQDDBNC
+echo ZWxsYSBSb21hIFBpenplcmllMRMwEQYDVQQKDApCZWxsYSBSb21hMQswCQYDVQQG
+echo EwJSTzAeFw0yNjA4MTcwODE5NTNaFw0zNjA4MTQwODE5NTNaMEAxHDAaBgNVBAMM
+echo E0JlbGxhIFJvbWEgUGl6emVyaWUxEzARBgNVBAoMCkJlbGxhIFJvbWExCzAJBgNV
+echo BAYTAlJPMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyIlOlsiH0rt8
+echo +x5TodLsyrlhKhDKVKqtRIbxKgr+U2AdNaT7fRAzDCaYvLYNgNxyjoBMBRt97UTI
+echo IUY35oHBVMq4AwVwJQEe81MxoLJpV8PerYsSUuoaV0hfnGQjTjHgSfLpZvABZ6XF
+echo D6VwCvg2u9JRsBDptdNaR1hIBmIwM7qNRill2LnAbqdVqOFIzd/sky1qywD+2/bi
+echo 54rBf0ml8SNgw/9V0hvSXeGGYgV/u2KN+HAWNt2/sKdwdurdchak7O5YqgNvqPx0
+echo 1JvkO/NhIGLd11LrbPbineNiXQuHwKQ1QVWQNcNHgIQ5JIFQuXNUMH8LZFs4KGZL
+echo nRL/7FpLOwIDAQABMA0GCSqGSIb3DQEBCwUAA4IBAQC7P039OQg3Zi/aZD8XHBkv
+echo 1SfuDeG3EgW5lqGdLib6cX1Ft/0XSIXYPIm/uMZrhn6owtEu9XP1nZGlt9u5ByMD
+echo GlsVdZX2EqBAhic4jQZa3N3rivpIHWTJfqMOE4eYYPrEXz3a4nmW5ZfW9Sp480qd
+echo gzPZLzVVwF7TtbmTyQ9gRE0c5mN/hHPvNTrCeGdqt2OjHfSVEVpvgwzvYI6yUftc
+echo V0+zyU4bzK51xmlBsofb1NfAjkLFj3oMNjCdMYTe36TC1sT0wbOYC262artO7dbj
+echo jA7Y2ztoFknFP/eGkUeXClaqcDqp1NHX3WWvZ5Eee3/qMGmmdiaqrQYwu6h2m7EF
 echo -----END CERTIFICATE-----
 ) > "%SSL_PATH%\digital-certificate.txt"
-echo       OK - Certificat instalat
+echo       OK - Certificat instalat cu succes!
 
-echo [4/4] Se reporneste QZ Tray...
+echo [3/3] Se reporneste QZ Tray...
 taskkill /f /im "qz-tray.exe" >nul 2>&1
-timeout /t 2 /nobreak >nul
-start "" "%ProgramFiles%\QZ Tray\qz-tray.exe" 2>nul
-if errorlevel 1 (
-    start "" "%ProgramFiles(x86)%\QZ Tray\qz-tray.exe" 2>nul
+timeout /t 3 /nobreak >nul
+
+:: Incearca mai multe locatii posibile
+if exist "%ProgramFiles%\QZ Tray\qz-tray.exe" (
+    start "" "%ProgramFiles%\QZ Tray\qz-tray.exe"
+    echo       OK - QZ Tray repornit din Program Files
+) else if exist "%ProgramFiles(x86)%\QZ Tray\qz-tray.exe" (
+    start "" "%ProgramFiles(x86)%\QZ Tray\qz-tray.exe"
+    echo       OK - QZ Tray repornit din Program Files x86
+) else if exist "%LOCALAPPDATA%\Programs\QZ Tray\qz-tray.exe" (
+    start "" "%LOCALAPPDATA%\Programs\QZ Tray\qz-tray.exe"
+    echo       OK - QZ Tray repornit din LocalAppData
+) else (
+    echo       ! Nu s-a gasit executabilul QZ Tray - porneste-l manual din Start Menu
 )
-echo       OK - QZ Tray repornit
 
 echo.
 echo ============================================
 echo   INSTALARE COMPLETA!
 echo ============================================
 echo.
-echo Deschide browserul si acceseaza:
+echo Acum deschide browserul si acceseaza:
 echo   https://proiect-pizzeria.vercel.app/receptie.html
 echo.
-echo Imprimanta va printa automat fara nicio fereastra!
+echo Imprimanta va printa automat fara pop-up!
 echo.
 pause
